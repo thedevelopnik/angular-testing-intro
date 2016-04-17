495a0cf0079d1cc36aeb272fa6aa65afc3d96413
David Sudia <dsudia@gmail.com>
2016-04-17 15:02:55 -0600

    added commented out  request to api to jump start that part of the learning process

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
+// var apiKey = '96b2d98e78db4b73a868ae4ef7b33cd0';
+//
+// $http({
+//   headers: { 'X-Auth-Token': apiKey },
+//   url: 'http://api.football-data.org/v1/soccerseasons/398/leagueTable',
+//   dataType: 'json',
+//   type: 'GET',
+// })
+
 var app = angular.module('soccerData', []);
